6ff941b52f5ae751791b5e6e8157830a8e498b45
alljp <allenjosesmailbox@gmail.com>
2017-08-04 12:08:34 +0530

    Add check_win() function
    adds win conditions and check if there is a win or not.
    return the winner, if there is.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -51,6 +51,18 @@ def update_board(board, move, turn):
     return board
 
 
+def check_win(board):
+    win_conditions = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
+                      (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for i in win_conditions:
+        try:
+            if board[i[0]] == board[i[1]] == board[i[2]]:
+                return board[i[0]]
+        except:
+            pass
+    return -1
+
+
 def main():
     print_instruction()
     board = [-1 for i in range(9)]
@@ -63,11 +75,9 @@ def main():
             turn = "X"
         else:
             turn = "O"
-
         user = get_move(turn)
         move += 1
         board = update_board(board, user, turn)
-        print_board(board)
 
 
 if __name__ == "__main__":
