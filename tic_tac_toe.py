d2561dd6f599ecfddbdeccab1a30123a899777b9
alljp <allenjosesmailbox@gmail.com>
2017-08-05 19:09:18 +0530

    Modify print_instrution to display thr board with cell numbers Seperate  print cell numbers from print_board function.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 
 
-def print_board(board=[2, 3, 4, 5, 6, 7, 8, 9, 10]):
-    print("\nThe game board: \n")
+def print_board(board):
     for i in range(3):
         print(" ", end="")
         for j in range(3):
@@ -10,11 +9,8 @@ def print_board(board=[2, 3, 4, 5, 6, 7, 8, 9, 10]):
                 print("X", end="")
             elif board[i*3+j] == 0:
                 print("O", end="")
-            elif board[i*3+j] != -1:
-                print(board[i*3+j]-1, end="")
             else:
                 print(" ", end="")
-
             if j != 2:
                 print(" | ", end="")
         print()
@@ -26,7 +22,17 @@ def print_board(board=[2, 3, 4, 5, 6, 7, 8, 9, 10]):
 
 def print_instruction():
     print("Please use the following cell numbers to make your move")
-    print_board()
+    for i in range(3):
+        print(" ", end="")
+        for j in range(3):
+            print(i*3+j+1, end="")
+            if j != 2:
+                print(" | ", end="")
+        print()
+        if i != 2:
+            print("-----------")
+        else:
+            print()
 
 
 def get_move(turn):
